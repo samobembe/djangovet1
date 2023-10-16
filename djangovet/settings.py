f83c402ca145e12782f5631e2b400227c9e014fa
samobembe <sam.obembe@datacenterdynamics.com>
2023-10-16 10:23:03 +0100

    added automatic AppConfig discovery

--- djangovet/settings.py
+++ djangovet/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Automatic AppConfig Discovery
+# https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
